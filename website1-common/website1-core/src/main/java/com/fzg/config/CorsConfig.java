304409fc2329e71dba90724d830e9f5f245affeb
yanjue12 <yanjue2024@163.com>
2025-07-14 11:39:02 +0800

    修改了跨域配置

--- website1-common/website1-core/src/main/java/com/fzg/config/CorsConfig.java
+++ website1-common/website1-core/src/main/java/com/fzg/config/CorsConfig.java
@@ -6,13 +6,17 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
+import javax.servlet.*;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 /**
  * 跨域配置类，用于配置允许跨域的规则
  */
 @Configuration
-public class CorsConfig {
+public class CorsConfig implements Filter {
 
-    @Bean
+    /*@Bean
     public CorsFilter corsFilter() {
         // 创建 CORS 配置对象
         CorsConfiguration config = new CorsConfiguration();
@@ -31,5 +35,27 @@ public class CorsConfig {
 
         // 创建并返回 CorsFilter 实例
         return new CorsFilter(source);
+    }*/
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        // 设置允许跨域的域名
+        HttpServletResponse res = (HttpServletResponse) response;
+        res.setHeader("Access-Control-Allow-Origin", "*");
+        res.setHeader("Access-Control-Allow-Methods", "*");
+        res.setHeader("Access-Control-Max-Age", "3600");
+        res.setHeader("Access-Control-Allow-Headers", "*");
+        res.setHeader("Access-Control-Allow-Credentials", "true");  // 允许携带凭证
+
+        chain.doFilter(request, response);
+
     }
+
+    public void init(FilterConfig filterConfig) throws ServletException {
+        // 初始化操作
+    }
+    public void destroy() {
+        // 销毁操作
+    }
+
 }
