af409a2d69d4ad17a1938a87ec15e513b2cb0d93
fengzg <fengzg@asiainfo.com>
2026-03-16 10:25:22 +0800

    ^TASK:20260106: 通知系统v3 增加导入依赖

--- website1-module-web/src/main/java/com/fzg/mapper/Notificationmapper.java
+++ website1-module-web/src/main/java/com/fzg/mapper/Notificationmapper.java
@@ -7,6 +7,9 @@ import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Map;
+
 @Repository
 public interface Notificationmapper extends BaseMapper<Notification> {
 
